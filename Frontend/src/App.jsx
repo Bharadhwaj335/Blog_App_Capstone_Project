78d8e08e82aa198c2bb1f8fef9fa7bd6c25edf8b
Sai Bharadhwaj <saimalyala7@gmail.com>
2026-04-06 21:03:54 +0530

    ..

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./components/Home";
 import UserProfile from "./components/UserProfile";
 import UserArticles from "./components/UserArticles";
 import AuthorProfile from "./components/AuthorProfile";
-import ArticleByID from "./components/ArticleByID";
+import ArticleByID from "./components/ArticleById";
 import AuthorArticles from "./components/AuthorArticles";
 import WriteArticle from "./components/WriteArticle";
 import { Toaster } from "react-hot-toast";
