a2442968195320dfbf5d13fc3b20201380cbe5ab
Sai Bharadhwaj <saimalyala7@gmail.com>
2026-05-11 11:37:03 +0530

    backend changes

--- Backend/server.js
+++ Backend/server.js
@@ -32,7 +32,8 @@ const connectDB = async () => {
     console.log("DB connection success");
 
     //start http server
-    app.listen(process.env.PORT, () => console.log(`server started on port ${process.env.PORT}`));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`server started on port ${PORT}`));
   } catch (err) {
     console.log("Err in DB connection", err);
   }
